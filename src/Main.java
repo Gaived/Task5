6326a7e0f21344f607d3d38a202c8240628208c5
Ivan Andreev <fc97@mail.ru>
2023-03-05 23:56:35 +0400

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.InputMismatchException;
 
+
 public class Main {
 
     private static void allP(List<String> list) {
@@ -20,21 +21,30 @@ public class Main {
         allP(list);
         try {
             int input = scanner.nextInt();
-            System.out.println("Покупка " + list.get(input-1) + " удалена, список покупок");
-            list.remove(input-1);
-            if (list.size() == 0) {
-                System.out.println("Список покупок пуст");
-            }
+            System.out.println("Покупка '" + list.get(input - 1) + "' удалена, список покупок");
+            list.remove(input - 1);
             scanner.nextLine();
         } catch (InputMismatchException exception) {
             String input = scanner.nextLine();
             System.out.println("Продукт '" + input + "' удален, список покупок");
             list.remove(input);
-            if (list.size() == 0) {
-                System.out.println("Список покупок пуст");
+        }
+        if (list.size() == 0) {
+            System.out.println("Список покупок пуст");
+        }
+    }
+
+    private static void searchP(List<String> list, Scanner scanner) {
+        String input = scanner.nextLine();
+        String tt = input.toLowerCase();
+        for (int i = 0; i < list.size(); i++) {
+            String itemLower = list.get(i).toLowerCase();
+            if (itemLower.contains(tt)) {
+                System.out.println(i + 1 + ". " + list.get(i));
             }
         }
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -45,9 +55,9 @@ public class Main {
             System.out.println("1. Показать список покупок");
             System.out.println("2. Добавить товар в спсок");
             System.out.println("3. Удбрать товар из списка");
+            System.out.println("4. Поиск товара по списку");
 
             String input = scanner.nextLine();
-
 
             switch (input) {
 
@@ -62,6 +72,10 @@ public class Main {
                 case "3":
                     System.out.println("Введите номер или название товара, который убрать из списка: ");
                     removeP(list, scanner);
+                    break;
+                case "4":
+                    System.out.println("Введите название продукта для поиска по списку: ");
+                    searchP(list, scanner);
                     break;
                 default:
                     System.out.println();
